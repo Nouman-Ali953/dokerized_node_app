52eeb1b9e74f2c0d8cd9aae5b7c796182c60d83b
Nouman-Ali953 <nauman.9533@gmail.com>
2025-11-27 23:18:25 +0500

    removed unused comment

--- app/src/index.js
+++ app/src/index.js
@@ -24,7 +24,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
-// Routes
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
